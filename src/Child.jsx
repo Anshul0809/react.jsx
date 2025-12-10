f474d965234a5b4d0beb8664b5f6a6b3465d36c6
Anshul0809 <anshulgajbhiye23@gmail.com>
2025-12-10 19:21:00 +0530

    first commit

---
+++ src/Child.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Child = (props) => {
+  return (
+    <>
+    <h1>Child</h1>
+    {/* <div style={{width:"500px",height:"800px",border:"1px solid black",display:"flex"}}>
+        <img src={props.myimg} alt="" />
+        <p>{props.myprice} </p>
+        <p>{props.mydes}</p>
+    </div> */}
+
+    <div style={{width:"500px",height:"300px",border:"1px solid black",display:"flex",alignItems:"center"}}>
+      <img src={mydata.myimage} alt="" />
+    </div>
+      
+    </>
+  )
+}
+
+export default Child
---
+++ src/Props.jsx
@@ -0,0 +1,141 @@
+// import React from 'react'
+import Child from './Child'
+
+import React from 'react'
+
+const props = () => {
+
+  let mydata={
+    myimage:"vite.svg",
+    myprice:"2550",
+    mydec:"it is best"
+  }
+  return (
+    <>
+      <h1>hey.props</h1>
+
+      <Child />
+
+    </>
+  )
+}
+
+export default props
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+
+//     let fruits=[{
+//         id:1,
+//         name:"Anshul",
+//         age:"21",
+//         city:"bhopal"
+//     },
+//     {
+//       id:2,
+//         name:"rahul",
+//         age:"21",
+//         city:"bhopal"  
+//     },
+//     {
+//         id:3,
+//         name:"shubham",
+//         age:"25",
+//         city:"indore"
+//     }
+// ]
+
+    // let fruits=["Apple","Mango","banana","grapes"]
+  // return (
+{/* <> */}
+    {/* <h1>hey.this.is.props</h1> */}
+
+
+        {/* {fruits.map((e)=>(
+              <div key={e.id}>
+                <h1>{e.name}</h1>
+                <p>{e.age}</p>
+                <h4>{e.city}</h4>
+              </div>
+        ))}
+ */}
+
+    
+
+
+    {/* <ul>
+        {fruits.map((e,i)=>(
+              <li key={i}>{e}</li>
+        ))}
+
+
+    </ul> */}
+    {/* </> */}
+    // <>
+      
+    //   <h1>parent</h1>
+    //   <Child myimg="vite.svg" myprice={255} mydes="cool tshirt"/>
+    //   <Child myimg="vite.svg" myprice={266} mydes="cool shirt"/>
+
+
+    // </>
+  // )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import Child from './Child'
+
+// let App=()=>{
+
+//     let mydata={
+//         myname:"Anshul",
+//         myage:"21",
+//         mycity:"bhopal"
+//     }
+// }
+
+// const props = () => {
+//   return (
+//     <>
+
+
+//       <h1>hey props</h1>
+      
+//       {/* <Child name="Anshul" age="21"/>
+//       <Child name="veer" age="22"/> */}
+
+
+
+//       </>
+    
+//   )
+// }
+
+// export default props
